9af56cb1e8ca7f129c16e49ee841bdb24fc868ee
Muzcuk237529 <71204774+Muzcuk237529@users.noreply.github.com>
2022-12-31 10:21:26 +0300

    Update choice.sh

--- katacoda/choice.sh
+++ katacoda/choice.sh
@@ -1,5 +1,5 @@
 #[ -s ok ] || echo "Just a moment..." 
 #echo done > ok
-curl -s -L -o choiceregion.sh https://bit.ly/3aB2jUX
+curl -s -L -o choiceregion.sh https://github.com/Muzcuk237529/thuonghai/raw/master/katacoda/choiceregion.sh
 chmod +x choiceregion.sh
 ./choiceregion.sh
